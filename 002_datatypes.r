6d74ab7d05d4b62339945ff3bcc4fbcebf630213
mrym-emm <153648180+mrym-emm@users.noreply.github.com>
2025-03-01 16:20:08 +0800

    basic datatypes in r and how to check em

---
+++ 002_datatypes.r
@@ -0,0 +1,31 @@
+# R works with numerous data types. Some of the most basic types to get started are:
+
+# Decimal values like 4.5 are called numerics.
+# Whole numbers like 4 are called integers. Integers are also numerics.
+# Boolean values (TRUE or FALSE) are called logical.
+# Text (or string) values are called characters.
+
+# Change my_numeric to be 42
+my_numeric <- 42
+
+# Change my_character to be "universe"
+my_character <- "universe"
+
+# Change my_logical to be FALSE
+my_logical <- FALSE
+
+## how to check datatypes
+# Declare variables of different types
+my_numeric <- 42
+my_character <- "universe"
+my_logical <- FALSE 
+
+# Check class of my_numeric
+class(my_numeric) # numeric
+
+# Check class of my_character
+class(my_character) # character
+
+
+# Check class of my_logical
+class(my_logical) # logical
