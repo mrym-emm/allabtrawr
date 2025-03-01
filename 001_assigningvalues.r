1c78c7354e13df99ff2d9629c0f4544dfcba2d6d
mrym-emm <153648180+mrym-emm@users.noreply.github.com>
2025-03-01 16:17:22 +0800

    how to assign values in r <-

---
+++ 001_assigningvalues.r
@@ -0,0 +1,24 @@
+# Assign the value 42 to x
+x <- 42
+
+# Print out the value of the variable x
+x
+
+# --------------------------------------------------------------------
+# Assign the value 5 to the variable my_apples
+my_apples <- 5
+
+# Print out the value of the variable my_apples
+my_apples
+
+# --------------------------------------------------------------------
+# Assign a value to the variables my_apples and my_oranges
+my_apples <- 5
+my_oranges <- 6
+
+
+# Add these two variables together
+my_fruit <- my_apples + my_oranges
+
+# Create the variable my_fruit
+my_fruit
